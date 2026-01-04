9188be1eb9731a34b6e38146dc8a6efba8a950e1
denkadev <denkadev@gmail.com>
2026-01-04 17:48:39 +0700

    feat: add key normalization and simple validation, and case sensitive flag

--- src/Env.php
+++ src/Env.php
@@ -2,6 +2,7 @@
 
 namespace Pastapen\Env;
 
+use InvalidArgumentException;
 use Pastapen\Env\Exceptions\ImmutableEnvironmentException;
 
 class Env
@@ -10,22 +11,50 @@ class Env
 
     protected bool $locked = false;
 
+    protected bool $caseSensitive = false;
+
+    public function __construct(bool $caseSensitive = false)
+    {
+        $this->caseSensitive = $caseSensitive;
+    }
+
     public function set(string $key, mixed $value): void
     {
         if($this->locked){
             throw new ImmutableEnvironmentException();
         }
+
+        $key = $this->normalizeKey($key);
         
         $this->values[$key] = $value;
     }
 
     public function get(string $key, mixed $default = null): mixed
     {
+        $key = $this->normalizeKey($key);
+
         return $this->values[$key] ?? $default;
+    }
+
+    protected function normalizeKey(string $key): string
+    {
+        $key = trim($key);
+
+        if(empty($key)){
+            throw new InvalidArgumentException("Environment key can't be empty");
+        }
+
+        if(!$this->caseSensitive){
+            $key = strtoupper($key);
+        }
+
+        return $key;
     }
 
     public function has(string $key): bool
     {
+        $key = $this->normalizeKey($key);
+
         return array_key_exists($key, $this->values);
     }
 
